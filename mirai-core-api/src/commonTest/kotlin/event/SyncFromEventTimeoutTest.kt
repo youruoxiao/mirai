a3a45b9b1a31d783c86dd90aeb22f79d679df4b5
Him188 <Him188@mamoe.net>
2022-01-06 04:28:02 +0000

    Move event tests from jvm to common

---
+++ mirai-core-api/src/commonTest/kotlin/event/SyncFromEventTimeoutTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019-2021 Mamoe Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+ *
+ * https://github.com/mamoe/mirai/blob/dev/LICENSE
+ */
+
+package net.mamoe.mirai.event
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+@Suppress("DEPRECATION")
+internal class SyncFromEventTimeoutTest : AbstractEventTest() {
+
+    @Test
+    fun testSyncFromEventTimeout() {
+        runBlockingWithTimeout(500) {
+            assertFailsWith<TimeoutCancellationException> {
+                syncFromEvent(100) { _: TestEvent -> }
+            }
+        }
+    }
+}
+
+internal fun <R> runBlockingWithTimeout(
+    millis: Long,
+    context: CoroutineContext = EmptyCoroutineContext,
+    block: suspend CoroutineScope.() -> R
+): R = runBlocking(context) {
+    withTimeout(millis, block)
+}
